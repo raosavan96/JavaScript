1382eac03a9777ffa06fa0ea8a8d2b1367dc5d92
raosavan9677 <savanyadav377@gmail.com>
2024-07-23 17:17:43 +0530

    hi

---
+++ js/dataTypes/datatyeps.js
@@ -0,0 +1,59 @@
+// DataTypes
+
+// 1. Strings DataTypes
+
+let a = "Sawan";
+
+console.log("strings datatyeps", a);
+
+
+// 2. Number
+
+let age = 23;
+
+console.log("number datatyeps", age);
+
+
+// 3. Boolean 
+
+let married = true;
+
+console.log("my married", married);
+
+
+// 4. Undefined DataTypes
+
+let undefined ;
+
+console.log(undefined);
+
+
+// 5. Array DataTypes
+
+let myArray = ["Sawan", 23, "Web Developer"];
+
+console.log(myArray);
+
+
+// 6. Object DataTypes
+
+let myObj = {
+    name: "Sawan Kumar Yadav", 
+    prof: "Web Developer",
+    age: 23, 
+    mail: "savanyadav377@gmail.com"
+}
+
+console.log(myObj);
+
+
+
+// DataTypes check 
+
+let my_name = "Sawan Kumar Yadav";
+let my_age = 23;
+let my_married = true;
+
+console.log(typeof(my_age));
+console.log(typeof(my_name));
+console.log(typeof(my_married));
