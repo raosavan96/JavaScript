8a470b675e777bd6928c8e67af5c311dc709b9a9
raosavan9677 <savanyadav377@gmail.com>
2024-07-24 14:19:49 +0530

    operator finish

--- js/operators/operators.js
+++ js/operators/operators.js
@@ -89,25 +89,124 @@
 
 // i. ( = )
 
-let a = 10;
-let b = 20;
-let c = 50;
+// let a = 10;
+// let b = 20;
+// let c = 50;
 
-console.log(a);
+// console.log(a);
 
-// ii. ( += )
+// // ii. ( += )
 
-let z = a += b;
-console.log(z);
+// let z = a += b;
+// console.log(z);
 
-c += 5;
+// c += 5;
 
-console.log(c);
+// console.log(c);
 
-// iii. ( -= )
+// // iii. ( -= )
 
-let k = a -= b;
-console.log(k);
+// let k = a -= b;
+// console.log(k);
 
-c -= 50;
-console.log(c);
+// c -= 50;
+// console.log(c);
+
+// // iv. ( *= )
+
+// let n = c *= a;
+// console.log(n);
+
+// // v. ( %= )
+
+// let v = 10;
+// let g = 2;
+
+// let u = v %= g;
+// console.log(u);
+
+// // vi. ( **= )
+
+// let l = v **= g;
+// console.log(l);
+
+
+
+// // 3. Comparison Opertors
+
+// // i. ( == )
+
+// let a = 5;
+// let b = 2;
+// let c = 5;
+// let d = "5"
+
+// console.log(a == b);
+// console.log(a == c);
+// console.log(a == d);
+
+// // ii. ( != )
+
+// console.log(a != b);
+// console.log(a != c);
+
+// // iii. ( === ) y string or number ko eual nhi krta h 
+
+// console.log(a === c);
+// console.log(a === d);
+
+// iv. ( !== )
+
+// console.log(a !== c);
+// console.log(a !== b);
+
+// // v. ( > )
+
+// console.log(a > b);
+// console.log(a > c);
+
+// // vi. ( < ) 
+
+// console.log(b < a);
+
+// // vii. ( < ) 
+
+// console.log(c >= a);
+
+// // vii. ( < ) 
+
+// console.log(c <= b);
+
+
+
+
+// // 4. Logical Operators
+
+// let a = 5;
+// let b = 5;
+// let c = 2;
+// let d = 2;
+
+// let x = a === b;
+// let y = a > b;
+// let z = c != a;
+// let n = a < d;
+
+// // i. logical end ( && )
+
+// console.log(x && z);
+// console.log(n && x);
+
+// // ii. logical or ( || )
+
+// console.log(x || n);
+// console.log(n || y);
+
+// // iii. logical not ( ! )
+
+let sawan = true;
+let yadav = false;
+
+console.log(!sawan);
+console.log(!yadav);
+
