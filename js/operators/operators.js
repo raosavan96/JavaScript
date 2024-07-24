591122f42da9d4ebd5ef93dbe8c1d9d79d672a40
raosavan9677 <savanyadav377@gmail.com>
2024-07-24 10:29:31 +0530

    Operator add

---
+++ js/operators/operators.js
@@ -0,0 +1,62 @@
+// Operators in JavaScript
+
+// 1. Arithmetic Operators
+
+// i. Addition Operator
+
+let x = 10;
+let y = 15;
+let a = "10";
+let b = "15";
+
+let z = x+y;
+let c = a+b;
+
+console.log(z);
+console.log(c);
+
+// ii. Subtraction Operator
+
+let d = y-x;
+console.log(d);
+
+let e = b-a;
+console.log(e);
+
+// iii. Multiplication Operator
+
+let h = x*y;
+console.log(h);
+
+let i = b*x;
+console.log(i);
+
+// iv. Division Operator
+
+let division = y/x;
+console.log(division);
+
+// v. Exponentiation Operator
+
+let j = 5**2
+
+console.log(j);
+
+// vi. Modulus (Remainder) Operator
+
+let s = 27;
+let ss = 2;
+
+let sss = s%ss;
+
+console.log(sss);
+
+// vii. Increment Operator
+
+let g = 29;
+    g++;
+console.log(g);
+
+// viii. Decrement Operator
+--g;
+console.log(g);
