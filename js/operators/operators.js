76e838aeb5c02393b8ae2e39369007cb4610031a
raosavan9677 <savanyadav377@gmail.com>
2024-07-24 13:27:15 +0530

    hi

--- js/operators/operators.js
+++ js/operators/operators.js
@@ -1,62 +1,113 @@
-// Operators in JavaScript
+// // Operators in JavaScript
 
-// 1. Arithmetic Operators
+// // 1. Arithmetic Operators
 
-// i. Addition Operator
+// // i. Addition Operator
 
-let x = 10;
-let y = 15;
-let a = "10";
-let b = "15";
+// let x = 10;
+// let y = 15;
+// let a = "10";
+// let b = "15";
 
-let z = x+y;
-let c = a+b;
+// let z = x + y;
+// let c = a + b;
 
+// console.log(z);
+// console.log(c);
+
+// // ii. Subtraction Operator
+
+// let d = y - x;
+// console.log(d);
+
+// let e = b - a;
+// console.log(e);
+
+// // iii. Multiplication Operator
+
+// let h = x * y;
+// console.log(h);
+
+// let i = b * x;
+// console.log(i);
+
+// // iv. Division Operator
+
+// let division = y / x;
+// console.log(division);
+
+// // v. Exponentiation Operator
+
+// let j = 5 ** 2
+
+// console.log(j);
+
+// // vi. Modulus (Remainder) Operator
+
+// let s = 27;
+// let ss = 2;
+
+// let sss = s % ss;
+
+// console.log(sss);
+
+
+// // Unary Operators
+
+// // i. Increment Operator
+
+// let g = 29;
+// let l = 20;
+
+// g++;
+// console.log(g);
+
+// // ii. Decrement Operator
+// g--;
+// console.log(g);
+
+// // (Post Increment & Decrement)
+
+// l++;
+// console.log(l);
+
+// g--;
+// console.log(g);
+
+// // (Pre Decrement & Increment)
+
+// --l;
+// console.log(l);
+
+// --g;
+// console.log(g);
+
+
+
+
+// 2. Assignment Operators
+
+// i. ( = )
+
+let a = 10;
+let b = 20;
+let c = 50;
+
+console.log(a);
+
+// ii. ( += )
+
+let z = a += b;
 console.log(z);
+
+c += 5;
+
 console.log(c);
 
-// ii. Subtraction Operator
+// iii. ( -= )
 
-let d = y-x;
-console.log(d);
+let k = a -= b;
+console.log(k);
 
-let e = b-a;
-console.log(e);
-
-// iii. Multiplication Operator
-
-let h = x*y;
-console.log(h);
-
-let i = b*x;
-console.log(i);
-
-// iv. Division Operator
-
-let division = y/x;
-console.log(division);
-
-// v. Exponentiation Operator
-
-let j = 5**2
-
-console.log(j);
-
-// vi. Modulus (Remainder) Operator
-
-let s = 27;
-let ss = 2;
-
-let sss = s%ss;
-
-console.log(sss);
-
-// vii. Increment Operator
-
-let g = 29;
-    g++;
-console.log(g);
-
-// viii. Decrement Operator
---g;
-console.log(g);
+c -= 50;
+console.log(c);
