b8b0ba6b42398792367839426d498dfcbf64000a
raosavan9677 <savanyadav377@gmail.com>
2024-07-25 22:31:28 +0530

    Strings Methods add

---
+++ js/stringsMethods/stringsMethods.js
@@ -0,0 +1,77 @@
+// // Strings Strings Methods in JavaScripts
+
+// // 1. length() methods
+
+// let str = "Sawan";
+
+// console.log(str.length);
+
+// // 2. slice() methods
+
+// let text = "Sawan Kumar Yadav";
+
+// let myCute = text.slice(6, 11);
+// console.log(myCute);
+
+// // 3. trim() methods
+
+// let text_2 = "    Hi aap kese ho   ";
+// let blanckSpece = text_2.trim();
+// console.log(blanckSpece);
+
+// // 4. toUpperCase() methods
+
+let myName = "sawan kumar yadav";
+
+let uppercase = myName.toUpperCase();
+console.log(uppercase);
+
+// 5. toLowerCase() methods
+
+let small = "SAWAN KUMAR YADAV";
+
+let lowercase = small.toLowerCase();
+
+console.log(lowercase);
+
+// 6. concat methods
+
+// exp_1
+
+let srt_1 = "Sawan";
+let srt_2 = " Yadav";
+
+console.log(srt_1.concat(srt_2));
+
+// exp_2
+
+let res = "Hello" + " World";
+console.log(res);
+
+// 7. replace() methods
+
+let srt = "Hello";
+let rep = srt.replace("H", "Y");
+console.log(rep);
+
+// 8. charAt() methods
+
+let fullName = "Sawan";
+let fullNamePrint = fullName.charAt(2);
+console.log(fullNamePrint);
+
+// // key value find
+
+// console.log(str[4]);
+
+// // Teplate leterals in js
+
+// // exp_1
+
+// let obj = {
+//     item: "Pen",
+//     price: "10"
+// }
+
+// let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
