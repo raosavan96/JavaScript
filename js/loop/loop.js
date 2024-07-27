5f9537aa6d4c4e5506df0f9634a33f22723769bf
raosavan9677 <savanyadav377@gmail.com>
2024-07-27 15:43:10 +0530

    loop update

---
+++ js/loop/loop.js
@@ -0,0 +1,21 @@
+// // loop in javaScript
+
+// // 1. for loop
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// array iteration
+
+let myArray = ["Dadi", "Ratan", "Sawan", "Lokesh", "Indra", "Nisha", "Manisha"];
+
+let size = myArray.length;
+console.log(size);
+
+for (let i = 0; i < size; i++) {
+//   console.log(i);
+  let indexP = myArray[i];
+  console.log(indexP);
+
+}
