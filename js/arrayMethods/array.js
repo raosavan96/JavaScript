8b7795bb2f1d5f57185e4e06824f48a735538dcb
raosavan9677 <savanyadav377@gmail.com>
2024-07-27 12:35:27 +0530

    array update

--- js/arrayMethods/array.js
+++ js/arrayMethods/array.js
@@ -5,7 +5,7 @@
 // let myArray = ["Sawan", "Yadav", "Web Developer", 23];
 // console.log(myArray);
 
-// // 2. array indices
+// // 2. array index checking
 
 // let marks = [97,26,88,66,57,82,51,84];
 // console.log(marks[7]);
@@ -19,7 +19,6 @@
 // console.log(output);
 // console.log(myArray);
 
-
 // // Array Methods in JavaScript
 
 // // 1. array length method
@@ -28,7 +27,6 @@
 
 // console.log(myArray.length);
 
-
 // 2. array toString() method
 
 // let myArray = ["Sawan", "Yadav", "Web Developer", 23];
@@ -36,7 +34,6 @@
 // let convertString = myArray.toString();
 
 // console.log(convertString);
-
 
 // // 3. array pop() method
 
@@ -48,8 +45,7 @@
 
 // console.log(myArray);
 
-
-// // 4. array push() method 
+// // 4. array push() method
 
 // // y method array ke last me new value add kr deta h
 
@@ -59,11 +55,61 @@
 
 // console.log(myArray);
 
+// // 5. arry join() method
 
-// 5. arry join() method
+// // y method array ki kays ko jo apne add krte h usse jod deta h
+// y method array ko strings me bi change kr deta h
+
+// let myArray = ["Sawan", "Yadav", "Web Developer", 23];
+
+// let output = myArray.join(",");
+
+// console.log(output + "dataTyeps :- " + typeof(output));
+// console.log(myArray);
+
+// // 6. array shift() method
+
+// let myArray = ["Sawan", "Yadav", "Web Developer", 23];
+// let removeFirst = myArray.shift();
+// console.log(myArray);
+// console.log("remove first array key :- " + removeFirst);
+
+// // 7. array unshift() methods
+
+// // y method array me start me new kay add kr deta h
+
+// let myArray = ["Sawan", "Yadav", "Web Developer", 23];
+// myArray.unshift("Rohit");
+// console.log(myArray);
+
+  
+// // 8. delete method in Array
+
+
+// let myArray = ["Sawan", "Yadav", "Web Developer", 23];
+
+// let deletA = delete myArray[2];
+
+// console.log(myArray);
+
+
+// // 9. concat() method in array
+
+// // 2 array ko ek sat jodne ka kam krta h
+
+// let myArray = ["Sawan", "Yadav", "Web Developer", 23];
+// let myNew = ["Rao", "Rohit", 12, "Nisha"];
+
+// let concatA = myArray.concat(myNew);
+// console.log(concatA);
+// let keyCheck = concatA[5];
+// console.log(keyCheck);
+
+
+// 10. splice() method in array
 
 let myArray = ["Sawan", "Yadav", "Web Developer", 23];
 
-let output = myArray.join("+");
+let spliceA = myArray.splice(3,1, "50");
 
-console.log(output);
+console.log(myArray);
