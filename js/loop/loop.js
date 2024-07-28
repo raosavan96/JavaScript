304727c0386f02c0af3fa9d266cb8640e013f3e7
raosavan9677 <savanyadav377@gmail.com>
2024-07-28 16:17:15 +0530

    hi loop

--- js/loop/loop.js
+++ js/loop/loop.js
@@ -1,20 +1,23 @@
-// // loop in javaScript
+// // // loop in javaScript
 
-// // 1. for loop
+// // // 1. for loop
 
-// for (let i = 0; i <= 5; i++) {
-//   console.log(i);
+// // for (let i = 0; i <= 5; i++) {
+// //   console.log(i);
+// // }
+
+// // array iteration
+
+// let myArray = ["Dadi", "Ratan", "Sawan", "Lokesh", "Indra", "Nisha", "Manisha"];
+
+// let size = myArray.length;
+// console.log(size);
+
+// for (let i = 0; i < size; i++) {
+// //   console.log(i);
+//   let indexP = myArray[i];
+//   console.log(indexP);
 // }
 
-// array iteration
 
-let myArray = ["Dadi", "Ratan", "Sawan", "Lokesh", "Indra", "Nisha", "Manisha"];
 
-let size = myArray.length;
-console.log(size);
-
-for (let i = 0; i < size; i++) {
-//   console.log(i);
-  let indexP = myArray[i];
-  console.log(indexP);
-}
