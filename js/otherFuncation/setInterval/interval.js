7decee0f90e78795ec8013ae6d3f4b0a4d9ba299
raosavan96 <savanyadav377@gmail.com>
2024-08-16 21:02:48 +0530

    alert

---
+++ js/otherFuncation/setInterval/interval.js
@@ -0,0 +1,7 @@
+// setInterval in javaScript
+
+function myFun() {
+  console.log("i am a setInterval");
+}
+
+setInterval(myFun, 3000);
---
+++ js/otherFuncation/setTimeout/settime.js
@@ -0,0 +1,8 @@
+// setTimeout in JavaScript
+
+function myFun() {
+    console.log("Hello i am a setTimeout");
+}
+
+
+setTimeout(myFun, 2000);
