ec67cecf990a86d06f3a2a45024bc63476ca03e8
raosavan96 <savanyadav377@gmail.com>
2024-08-16 12:52:31 +0530

    scop

---
+++ js/scop/scop.js
@@ -0,0 +1,49 @@
+// // scop
+
+// // 1. global scop
+
+// var a = 10;
+
+// {
+//   var b = 10;
+// }
+
+// console.log(a);
+// console.log(b);
+
+// // 2. block scop
+
+// let c = 11;
+
+// {
+//   const d = 12;
+//   console.log(d)
+// }
+
+// console.log(c);
+
+
+// 3. local scop
+
+// (1)
+
+// function add(a,b) {
+//    return a*b;
+// }
+
+// let c = add(3,3);
+// console.log(c);
+
+// (2)
+
+function kjkj (){
+    let a = "sawan"
+    return a;
+}
+
+console.log(kjkj());
+
+
+
+
+
