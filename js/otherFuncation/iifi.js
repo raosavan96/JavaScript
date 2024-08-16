c58767b3ce382d1d7c6bab437f337c3316159f43
raosavan96 <savanyadav377@gmail.com>
2024-08-16 20:42:52 +0530

    iifi

---
+++ js/otherFuncation/iifi.js
@@ -0,0 +1,14 @@
+// iifi in javaScript
+
+// (1)
+
+(function (a, b) {
+  console.log(a + b);
+  //   console.log("funcation");
+})(10, 10);
+
+// (2)
+
+((c, d) => {
+  console.log(c * d);
+})(5, 5);
