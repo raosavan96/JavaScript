b4c6551d21e8cdef3275164214600c6b10aae658
raosavan96 <savanyadav377@gmail.com>
2024-08-17 13:08:35 +0530

    hoisting

---
+++ js/Set/set.js
@@ -0,0 +1,35 @@
+// Set in javaScript
+
+// - esme uniq value add krte h ;
+// - y kisi key value ko dubara store ni krta h
+
+// let mySets = new Set(["a", "b", "a", "c", "d", "m", "m"]);
+// console.log(mySets);
+
+// Set Methods - value add krna
+
+// 1. add()
+
+// let set2 = new Set([]);
+
+// set2.add("Sawan");
+// set2.add("Yadav");
+// set2.add("Sawan");
+
+// console.log(set2);
+
+// 2. has() - value fine krne ke kam aata h
+
+// let mySets = new Set(["Sawan", "Yadav", "Lokesh", "Nisha", "Manisha"]);
+
+//  let find = mySets.has("Nisha");
+
+// console.log(find)
+
+// 3. delete()
+
+let mySets = new Set(["Sawan", "Yadav", "Lokesh", "Nisha", "Manisha"]);
+
+mySets.delete("Sawan");
+
+console.log(mySets);
