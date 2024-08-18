38c25e881d4c6c78d6f276bad5ce59df9ba775d9
raosavan96 <savanyadav377@gmail.com>
2024-08-18 09:55:30 +0530

    math & methods prectice

--- js/Math/mathMethods.js
+++ js/Math/mathMethods.js
@@ -43,5 +43,44 @@
 
 // prectice
 
-let a = Math.floor(Math.random() * 10);
-console.log(a);
+// let a = Math.floor(Math.random() * 10);
+// console.log(a);
+
+// let a = Math.PI;
+// console.log(a);
+
+// function myFun(length) {
+//   let num = "1234567890";
+//   let opt = 0;
+//   for (let i = 1; i <= length; i++) {
+//     let renNum = Math.floor(Math.random() * num.length);
+//     opt += num.charAt(renNum);
+//   }
+//   console.log(opt);
+// }
+
+// myFun(6);
+
+// let a = 8.4;
+// let b = 8.6;
+// let c = Math.round(a);
+// let d = Math.round(b)
+// console.log(c)
+// console.log(d)
+
+// let a = 8.4;
+// let b = 8.6;
+
+// let c = Math.floor(a);
+// console.log(c);
+// let d = Math.ceil(a);
+// console.log(d);
+
+// let a = Math.sqrt(25);
+
+// console.log(a);
+
+let a = -24353;
+
+let b = Math.abs(a);
+console.log(b);
